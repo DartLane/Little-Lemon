8ff14920ba7bfefb7ac6e878ee27a3f2ff968f03
DartLane <asilva200499@gmail.com>
2025-02-20 20:26:06 +0000

    Add custom typography with Google Fonts

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,10 @@ import './Main.css';
 
 function Main() {
     return (
-        <main className="main"></main>
+        <main className="main">
+            <h1>Title</h1>
+            <p>Paragraph text</p>
+        </main>
     );
 }
 
