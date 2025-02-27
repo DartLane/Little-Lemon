358b91b4d65090204897658893bd67c92d06e102
DartLane <asilva200499@gmail.com>
2025-02-27 18:08:52 +0000

    Add Booking Information section with styling

---
+++ src/components/BookingInformation.js
@@ -0,0 +1,16 @@
+import './BookingInformation.css';
+
+function BookingInformation() {
+    return (
+        <section className='booking-information-section'>
+            <section className='booking-information-container'>
+                <h2>Book a table at Little Lemon</h2>
+                <h1>Make a restaurant reservation</h1>
+                <span>To make a table booking, please fill out the reservation form below.</span><br/>
+                <span>Reservations are available up to 6 months in advance.</span>
+            </section>
+        </section>
+    );
+}
+
+export default BookingInformation;
